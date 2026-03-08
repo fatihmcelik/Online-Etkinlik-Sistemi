a74174fd1944448a8dc59522736838e47e269b82
Fatih Muhammed Celik <fatihcelik112112@gmail.com>
2026-03-08 14:06:21 +0300

    Proje iskeleti oluşturuldu

---
+++ src/test/java/com/etkinlik/online_etkinlik/OnlineEtkinlikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etkinlik.online_etkinlik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineEtkinlikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
